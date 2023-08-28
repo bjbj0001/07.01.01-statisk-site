555d76e570bc04fce7f4e187a97b77e2eeddb926
BjarneyMaria <bjarneymaria@gmail.com>
2023-08-28 15:53:22 +0200

    js works with sold out

--- produktliste.js
+++ produktliste.js
@@ -19,13 +19,17 @@ function showProduct(product) {
 
   //ændre indhold
   copy.querySelector("h3").textContent = product.productdisplayname;
-  //copy.querySelector("p.price").textContent = product.price;
   copy.querySelector(".articletype").textContent = product.articletype;
   copy.querySelector(".brandname").textContent = product.brandname;
   copy.querySelector(".price span").textContent = product.price;
   copy.querySelector(".finalprice").textContent = total;
   copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  //Udsolgt produkt
+  if (product.soldout) {
+    copy.querySelector("article").classList.add("soldOut");
+  }
 
+  //copy.querySelector(".soldOut").textContent = souldout;
   //appende
   document.querySelector("main").appendChild(copy);
 }
